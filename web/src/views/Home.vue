0702c30fa32662c095904e2f3a38cace86c66dde
zhanghao <zh975525512@gmail.com>
2023-06-21 10:18:58 +0800

    init project

---
+++ web/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <InitVue />
+</template>
+
+<script>
+export default {
+    name: 'HomeVue',
+}
+</script>
+
+<script setup>
+import InitVue from '@/components/Init.vue';
+</script>
+
+<style></style>
