a21024e35ff075f14c1affa6f6e14fd27c48b1db
zhishi <2500561808@qq.com>
2023-06-28 14:10:36 +0800

    多图植物信息页

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <HomeVue />
+  <router-view></router-view>
 </template>
 
 <script>
@@ -8,23 +8,9 @@ export default {
 }
 </script>
 
-<script setup>
-import HomeVue from '@/views/Home.vue'
-</script>
-
 <style>
 #app {
-  width: 100vw;
-  height: 100vh;
-  overflow: hidden;
-  background-image: url("assets/bg1.jpg");
-/*linear-gradient(*/
-/*rgba(0, 0, 0, 0.9),*/
-/*rgba(0, 0, 0, 0.9) 60%,*/
-/*rgba(34, 34, 34, 0.1)*/
-/*),*/
-  background-repeat: no-repeat;
-  background-size:100%;
+
 }
 body{
   margin: 0; /* 外边距 */
