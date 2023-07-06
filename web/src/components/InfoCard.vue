e58e3f9fd3357534024c88acf00e7d692c170ddd
zhanghao <mloserwork@gmail.com>
2023-07-06 10:30:53 +0800

    temp

--- web/src/components/InfoCard.vue
+++ web/src/components/InfoCard.vue
@@ -1,24 +1,26 @@
 <template>
     <div style="background: #d7edde;height:100vh;width: 99vw;border-radius: 5px;margin: 0px auto;overflow-y: scroll;">
-      <div style="padding: 4vh 6vw 2vh;width: 100vw">
-        <p style="font-size: 8vh;text-align: left"> {{ props.plant.flower_name }}</p>
-        <p style="font-size: 2.5vh;color: darkgreen;text-align: left;margin-top: -25px;margin-left: 20px;">{{ props.plant.flower_type }}</p>
-        <div style="padding-top: 24px;text-align: left">
-          <img :src="props.plant.pics[0]" class="flower_img">
-          <img :src="props.plant.pics[1]" class="flower_img">
-          <img :src="props.plant.pics[2]" class="flower_img">
+        <div style="padding: 4vh 6vw 2vh;width: 100vw">
+            <p style="font-size: 8vh;text-align: left"> {{ props.plant.flower_name }}</p>
+            <p style="font-size: 2.5vh;color: darkgreen;text-align: left;margin-top: -25px;margin-left: 20px;">{{
+                props.plant.flower_type }}</p>
+            <div style="padding-top: 24px;text-align: left">
+                <img :src="props.plant.pics[0]" class="flower_img">
+                <img :src="props.plant.pics[1]" class="flower_img">
+                <img :src="props.plant.pics[2]" class="flower_img">
+            </div>
         </div>
-      </div>
-      <div style="
+        <div style="
   height: 100vh;
   top: 10vh;
   position: relative;
   background: whitesmoke;
   padding: 5vh 6vw;
   font-size: 2.5vh;
-  line-height: 1.4;">
-          {{ props.plant.flower_intro }}
-      </div>
+  line-height: 1.4;
+  white-space: pre-wrap;">
+            {{ props.plant.flower_intro }}
+        </div>
     </div>
 </template>
 
